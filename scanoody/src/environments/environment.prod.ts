702b25573488c3f5e10263078fe91626310459ae
Kesherwani <vkesherw@visteon.com>
2019-11-17 17:29:56 +0100

    app setup

---
+++ scanoody/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyDSgaRmh2MRJKvRwRdMTEVAvotsknV56KE",
+    authDomain: "scanoody.firebaseapp.com",
+    databaseURL: "https://scanoody.firebaseio.com",
+    projectId: "scanoody",
+    storageBucket: "scanoody.appspot.com",
+    messagingSenderId: "585872612689",
+    appId: "1:585872612689:web:ab997d5aecbfaef78dc770",
+    measurementId: "G-3DHBPJMV2F"
+  }
+};
---
+++ scanoody/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false, 
+  firebaseConfig:{
+    apiKey: "AIzaSyDSgaRmh2MRJKvRwRdMTEVAvotsknV56KE",
+    authDomain: "scanoody.firebaseapp.com",
+    databaseURL: "https://scanoody.firebaseio.com",
+    projectId: "scanoody",
+    storageBucket: "scanoody.appspot.com",
+    messagingSenderId: "585872612689",
+    appId: "1:585872612689:web:ab997d5aecbfaef78dc770",
+    measurementId: "G-3DHBPJMV2F"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
